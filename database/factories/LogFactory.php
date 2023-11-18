1f74e0bcd44f65b7691ed47be730299bd5327fba
Hemant D <50235905+h3mantD@users.noreply.github.com>
2023-11-18 20:27:03 +0530

    Add LogFactory and LogSeeder

--- database/factories/LogFactory.php
+++ database/factories/LogFactory.php
@@ -1,13 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Nette\Utils\Random;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Log>
  */
-class LogFactory extends Factory
+final class LogFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +20,23 @@ class LogFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'level' => $this->faker->randomElement(
+                array: ['debug', 'info', 'notice', 'warning', 'error', 'critical', 'alert', 'emergency']
+            ),
+            'message' => $this->faker->sentence,
+            'resourceId' => 'server-' . Random::generate(length: 4, charlist: '0-9'),
+            'timestamp' => $this->faker->dateTimeThisYear->format(format: 'Y-m-d\TH:i:s\Z'),
+            'traceId' => Random::generate(length: 3, charlist: 'a-z') . '-' .
+                Random::generate(length: 3, charlist: 'a-z') . '-' .
+                Random::generate(length: 3, charlist: '0-9'),
+            'spanId' => 'span-' . Random::generate(length: 3, charlist: '0-9'),
+            'commit' => $this->faker->sha1,
+            'metadata' => [
+                'ip' => $this->faker->ipv4,
+                'userAgent' => $this->faker->userAgent,
+                'referer' => $this->faker->url,
+                'parentResourceId' => 'server-' . Random::generate(length: 4, charlist: '0-9'),
+            ],
         ];
     }
 }
--- database/seeders/LogSeeder.php
+++ database/seeders/LogSeeder.php
@@ -1,17 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class LogSeeder extends Seeder
+final class LogSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        \App\Models\Log::factory()
+            ->count(100)
+            ->create();
     }
 }
